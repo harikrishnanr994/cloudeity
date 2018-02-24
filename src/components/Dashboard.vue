2e33670ec753bc934a0d1c45ab0716dc03987083
Sachin Giridhar <sachingiridhar@gmail.com>
2018-02-24 10:37:00 +0530

    Launch Button click functionality given

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,7 +10,9 @@
                 <p class="para">To launch a new website using wordpress</p>
               </el-col>
               <el-col :xs="24" :sm="6" :md="4" :lg="4" :xl="2" class="center">
+              <router-link to="/paid" exact>
                 <el-button type="primary" class="button" plain>Launch</el-button>
+              </router-link>
               </el-col>
             </el-row>
         </div>
@@ -22,7 +24,9 @@
                 <p class="para">To launch a new free website using wordpress</p>
               </el-col>
               <el-col :xs="24" :sm="6" :md="4" :lg="4" :xl="2" class="center">
+              <router-link to="/free" exact>
                 <el-button type="primary" class="button" plain>Launch</el-button>
+              </router-link>
               </el-col>
             </el-row>
         </div>
